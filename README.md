3c6bfa8d7b50a9ef206d44cf3564587bcd766040
Damian Polak <dpolak@dtiq.com>
2023-07-16 18:36:00 +0200

    added README content, added AttentionBlock3D, removed MriNet (moved to another repo)

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # pseudo3d-pytorch
 Pseudo-3D CNN networks in PyTorch.
+
+This repository contains my own (unofficial) implementation of Pseudo-3D convolutional networks from the paper "Learning
+Spatio-Temporal Representation with Pseudo-3D Residual Networks" (https://arxiv.org/abs/1711.10305). 
+
+Precise description of the architecture can be found in the paper. Detailed description of the P3D blocks can be
+found in the paper as well.
+
+Additionally, I have implemented 3D version of (CBAM - Bottleneck Attention Module) from "Convolutional Block Attention
+Module" paper. (https://openaccess.thecvf.com/content_ECCV_2018/html/Sanghyun_Woo_Convolutional_Block_Attention_ECCV_2018_paper.html).
+
+This implementation extends original CBAM module to 3D. In this case, the spatial attention is computed
+along the temporal dimension (dim=2). Then, mean and max features are concatenated in the filter dimension (dim=1),
+like in the original CBAM module. It is because MRI scans are 1-channel 3D volumes, and important features can be
+extracted from the temporal dimension in this case.
+
+## Usage
+Repository contains implementation of P3D blocks (type A, B, and C) and P3D ResNet-like networks. To use them, you can
+import them from `p3d_resnet.py` file and use P3DResNet class.
+
+Model size is parametrized - you can choose between 
+every ResNet size using num_blocks_per_stage parameter. Default is `(3, 4, 6, 3)`, which corresponds to the ResNet-50
+architecture. For ResNet-101, use `(3, 4, 23, 3)`. For ResNet-152, use `(3, 8, 36, 3)`. 
+
+You can also build your own P3D network using P3DBlockA, P3DBlockB, and P3DBlockC classes from `src/blocks.py` file. 
+This file also contains implementation of Attention module (`AttentionBlock3D` class).
+
+Blocks and P3D models are implemented using PyTorch's `nn.Module` class, so you can use them as any other PyTorch
+module. Parameters of these classes are described in the docstrings.
+
+Repository also contains tests in `tests` directory. You can run them using `pytest` command. 
+
+Repository is compatible with Python 3.10 and PyTorch 2.0.1.
--- src/blocks.py
+++ src/blocks.py
@@ -199,3 +199,62 @@ class P3DBlockTypeC(P3DBlock):
         x = self.relu(x)
 
         return x
+
+
+class AttentionBlock3D(nn.Module):
+    """
+    Attention block (CBAM - Bottleneck Attention Module) for 3D image data. This is implementation of the
+    "Convolutional Block Attention Module" paper. (https://openaccess.thecvf.com/content_ECCV_2018/html/
+    Sanghyun_Woo_Convolutional_Block_Attention_ECCV_2018_paper.html).
+
+    This implementation extends original CBAM module to 3D. In this case, the spatial attention is computed
+    along the temporal dimension (dim=2). Then, mean and max features are concatenated in the filter dimension (dim=1),
+    like in the original CBAM module. It is because MRI scans are 1-channel 3D volumes, and important features can be
+    extracted from the temporal dimension in this case.
+    """
+
+    def __init__(self, num_filters: int, reduction_ratio: int = 4) -> None:
+        """
+        Initialize the attention block.
+
+        :param num_filters: number of input filters.
+        :param reduction_ratio: reduction ratio of the channel attention.
+        """
+        super().__init__()
+
+        self.shared_mlp_channel_attention = nn.Sequential(
+            nn.Conv3d(num_filters, num_filters // reduction_ratio, kernel_size=1, stride=1, padding="same", bias=False),
+            nn.ReLU(),
+            nn.Conv3d(num_filters // reduction_ratio, num_filters, kernel_size=1, stride=1, padding="same", bias=False)
+        )
+
+        self.conv_spatial_attention = nn.Conv3d(num_filters * 2, 1, kernel_size=7, stride=1, padding="same", bias=False)
+
+        self.channel_avg_pool = nn.AdaptiveAvgPool3d((1, 1, 1))
+        self.channel_max_pool = nn.AdaptiveMaxPool3d((1, 1, 1))
+        self.sigmoid = nn.Sigmoid()
+
+    def forward_spatial_attention(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass of the spatial attention."""
+        avg_out = torch.mean(x, dim=2, keepdim=True)
+        max_out, _ = torch.max(x, dim=2, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv_spatial_attention(x)
+        x = self.sigmoid(x)
+        return x
+
+    def forward_channel_attention(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass of the channel attention."""
+        x_avg = self.channel_avg_pool(x)
+        x_max = self.channel_max_pool(x)
+        x = self.shared_mlp_channel_attention(x_avg) + self.shared_mlp_channel_attention(x_max)
+        x = self.sigmoid(x)
+        return x
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass of the attention block."""
+        identity = x
+        x = self.forward_channel_attention(x) * x
+        x = self.forward_spatial_attention(x) * x
+        x = x + identity
+        return x
